fae9460ccf5e16fb5574c897dd426dfbd4d5dfd6
liuixn <963875368@qq.com>
2022-07-04 18:31:30 +0800

    nio buffer学习

---
+++ netty/src/main/java/com/liuxin/java/nio/buffer/DuplicateBuffer.java
@@ -0,0 +1,16 @@
+package com.liuxin.java.nio.buffer;
+
+import java.nio.CharBuffer;
+
+public class CopyBuffer {
+    public static void main(String[] args) {
+        CharBuffer allocate = CharBuffer.allocate(100);
+        allocate.duplicate()
+    }
+}
+
+/*
+* CharBuffer allocate = CharBuffer.allocate(100);
+  allocate.duplicate()
+*
+* */
