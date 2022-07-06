eb07abf66ca19e616ff0a3ef139b3b7c23808022
liuixn <963875368@qq.com>
2022-07-06 19:24:07 +0800

    channel to channel

--- netty/src/main/java/com/liuxin/java/nio/channel/ChannelToChannel.java
+++ netty/src/main/java/com/liuxin/java/nio/channel/ChannelToChannel.java
@@ -1,4 +1,39 @@
 package com.liuxin.java.nio.channel;
 
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.WritableByteChannel;
+import java.util.Objects;
+
 public class ChannelToChannel {
+    public static void main(String[] args) {
+        String rootPath = FileChannelCus2.class.getClassLoader().getResource(".").getPath();
+        String filename = "file";
+        String completePath = rootPath + filename;
+        RandomAccessFile accessFile = null;
+        FileChannel channel = null;
+        FileLock lock = null;
+        WritableByteChannel writableByteChannel = Channels.newChannel(System.out);
+        try {
+            accessFile = new RandomAccessFile(completePath, "rw");
+            channel = accessFile.getChannel();
+            channel.transferTo(0, channel.size(), writableByteChannel);//传输到
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (Objects.nonNull(lock)) lock.release();
+                if (Objects.nonNull(accessFile)) accessFile.close();
+                if (Objects.nonNull(channel)) channel.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
